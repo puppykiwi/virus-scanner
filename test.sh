1ebcce3ab0fe8dc104a10661ed61b4ced3f697bd
puppykiwi <johnraymwendwa04@gmail.com>
2025-01-28 17:58:51 +0300

    bash

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request GET \
+     --url https://www.virustotal.com/api/v3/files/ZmY4Yjc2YmRmN2MzNmM5NDFkMmY1ZDA4NDA5YWQ1MDg6MTczODA3MzY2NA%3D%3D \
+     --header 'accept: application/json' \
+     --header 'x-apikey: f6335ba146578f6a07ddf8e11af1966423366278fda02b750262b4833bcbfedc'
